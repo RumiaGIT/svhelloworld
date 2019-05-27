600221bcee67c09423e1072cc14e07314c3e5848
Jesse Kramer <jkramer@pcextreme.nl>
2019-05-27 08:45:17 +0200

    Built resources

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -1,42 +1,22 @@
 @extends('layouts.master')
-@section('title', 'Aanmelden activiteiten')
+@section('title', 'Activiteiten')
 
 @section('content')
-    <h3>Activiteiten</h3>
     @if ($activities->count())
-        <p>Dit is een overzicht van de activiteiten die georganiseerd worden door Studievereniging "Hello World".</p>
-
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Naam</th>
-                        <th>Datum en tijd</th>
-                        <th>Aanmeldperiode</th>
-                        <th>Acties</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($activities as $activity)
-                        <tr>
-                            <td>
-                                <a href="{{ route('activity.show', $activity->id) }}">{{ $activity->title }}</a>
-                            </td>
-                            <td>
-                                @datetime($activity->starts_at) t/m @datetime($activity->ends_at)
-                            </td>
-                            <td>
-                                @date($activity->available_from) t/m @date($activity->available_to)
-                            </td>
-                            <td>
-                                <a href="{{ route('activity.show', $activity->id) }}" class="btn btn-primary btn-xs">Bekijken</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="row">
+            @foreach($activities as $activity)
+                <div class="col-lg-4 col-md-6 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <h3 class="m-b-0">{{ $activity->title }}</h3>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     @else
-        <p class="alert alert-info">Op dit moment zijn er geen activiteiten beschikbaar, kijk op een later moment nog eens!</p>
+        <p class="text-center text-info">
+            Op dit moment zijn er geen activiteiten beschikbaar, kijk op een later moment nog eens!
+        </p>
     @endif
 @endsection
